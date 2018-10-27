9b228120ec96713ccfc7771075fd291aa865242a
Godwin Anasigre <godwin.anasigre@hp.com>
2018-10-27 09:01:08 -0500

    demo application initial commit

---
+++ src/main/java/com/godwin/demo/application/GodwinDemoApplication.java
@@ -0,0 +1,12 @@
+package com.godwin.demo.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodwinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodwinDemoApplication.class, args);
+	}
+}
